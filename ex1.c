c1c290f5e72991c34275522dd46d48a58436b6e5
DrifterDave <drifterdave@users.noreply.github.com>
2017-06-09 21:07:16 -0400

    Initial commit. Exercises 1, 2, and 3 completed.

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+/* this is a comment. */
+int main(int argc, char *argv[])
+{
+	int distance =  400;
+	// This is an inline commet
+	printf("You are %d miles away.\n", distance);
+
+	return 0;
+}
